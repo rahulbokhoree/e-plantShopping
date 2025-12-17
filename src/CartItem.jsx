20ee54fb64aad3752890afe46977c550d80c50ee
Rahul Bokhoree <rahulbokhoree@trentu.ca>
2025-12-16 19:06:38 -0500

    Checkout button alert

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -26,6 +26,7 @@ const CartItem = ({ onContinueShopping }) => {
     }
   };
 
+  //This needs later development
   const handleCheckoutShopping = (e) => {
     alert('Functionality to be added for future reference');
   };
@@ -88,7 +89,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
